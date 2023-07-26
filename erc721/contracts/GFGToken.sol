590db4ef1db7de386c0d96cea1e919374c25060b
yoshinao <yoshinao@fit.cvut.cz>
2023-07-26 10:40:12 +0900

    feat: add NFT examples

--- erc721/contracts/GFGToken.sol
+++ erc721/contracts/GFGToken.sol
@@ -1,31 +1,30 @@
-// SPDX-License-Identifier: MIT
-
-/**
- * Author: Lawrence Onah <paplow01@gmail.com>
- * Github: https://github.com/kodjunkie
- */
-
-pragma solidity ^0.8.1;
-
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.6 <0.9.0; 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-
+/// @title A contract for demonstrate ERC-721 Token
+/// @author Jitendra Kumar
+/// @notice For now, this contract just show how to create an ERC-721 Token
 contract GFGToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
  
     Counters.Counter private _tokenCountCounter;
+    event Minted(address owner, uint256 tokenId);
  
     //set the NFT name and symbol
     constructor() ERC721("GFGToken", "GFG") {}
     //mint the NFT using Owner Address
-    function safeMint(string memory _uri) public onlyOwner {
+    function safeMint(string memory _uri) public onlyOwner returns(uint256){
         uint256 tokenCount = _tokenCountCounter.current();
         _tokenCountCounter.increment();
         _safeMint(msg.sender, tokenCount);
         _setTokenURI(tokenCount, _uri);
+
+          emit Minted(msg.sender, tokenCount);  // Emit the event
+        return tokenCount;
     }
      
     function _beforeTokenTransfer(address from, address to, uint256 tokenCount, uint256 batchSize)
--- erc721/test/GFGToken-test.js
+++ erc721/test/GFGToken-test.js
@@ -1,36 +1,70 @@
+// test/GFGToken.test.js
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("GFGToken", function() {
-  let GFGToken, gfgToken, owner, addr1;
-  
-  beforeEach(async function () {
-    GFGToken = await ethers.getContractFactory("GFGToken");
+  let GFGToken;
+  let gfgToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
-    [owner] = await ethers.getSigners();
-    
+  beforeEach(async function() {
+    GFGToken = await ethers.getContractFactory("GFGToken");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
     gfgToken = await GFGToken.deploy();
     await gfgToken.deployed();
+  });
+
+  describe("Deployment", function() {
+    it("Should set the right owner", async function() {
+      expect(await gfgToken.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign the total supply of tokens to the owner", async function() {
+      const ownerBalance = await gfgToken.balanceOf(owner.address);
+      expect((await gfgToken.totalSupply()).toString()).to.equal(ownerBalance.toString());
+    });
+  });
+
+  describe("Transactions", function() {
+    it("Should fail if sender doesn’t have enough tokens", async function() {
+      const initialOwnerBalance = await gfgToken.balanceOf(owner.address);
+
+      // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens).
+      // `require` will evaluate false and revert the transaction.
+      let reverted = false; 
+      try{
+        await gfgToken.connect(addr1).transferFrom(addr1.address, owner.address, 1);
+      } catch(e){
+        // console.log(e);
+        reverted = true;
+      }
+      expect(reverted).to.be.true;
+
+      // Owner balance shouldn't have changed.
+      expect((await gfgToken.balanceOf(owner.address)).toString()).to.equal(
+        initialOwnerBalance.toString()
+      );
+    });
+
+    it("Should update balances after transfers", async function() {
+      // Mint a new token and capture the emitted event.
+      let mintTx = await gfgToken.connect(owner).safeMint("ipfs://uri");
+      let receipt = await mintTx.wait();
+      let event = receipt.events.filter((x) => {return x.event == "Minted"});
+      let tokenId = event[0].args.tokenId;
     
-    [addr1] = await ethers.getSigners();
-  });
-
-  it("Should safemint", async function() {
+      // Transfer from owner to addr1.
+      await gfgToken.connect(owner).transferFrom(owner.address, addr1.address, tokenId);
     
-    await gfgToken.connect(owner).safeMint("https://meditationduck.com/1");
-
-    expect(await gfgToken.ownerOf(0)).to.eq(owner.address);
-    expect(await gfgToken.tokenURI(0)).to.eq("https://meditationduck.com/1");
+      expect((await gfgToken.balanceOf(owner.address)).toString()).to.equal("0")
+      expect((await gfgToken.balanceOf(addr1.address)).toString()).to.equal("1");
+      
+      // Check token URI
+      expect(await gfgToken.tokenURI(tokenId)).to.equal("ipfs://uri");
+    });
   });
-
-  it("Should only allow owner to mint", async function() {
-    await expect(
-      gfgToken.connect(addr1).safeMint("https://meditationduck.com/1").catch((err) => {
-        throw new Error(err.message);
-      })
-    ).to.be.eventually.throw("Ownable: caller is not the owner");
-  });
-  
-
-  
 });
